0c33078f29a60f33fc1e3a7ff7c1e098eabf82a2
Courtney Fay <fay.courtney@gmail.com>
2017-09-20 16:52:51 -0600

    adds all files creates basic html board game and styles simple css

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+3. JavaScript portion will be next:
+	3a. Locate the element first to use it within your app. Think about using document.getElementById, document.getElementsByClassName or something similar to locate your target elements. Try this in your console to make sure your selection works.
+	3b. After finding the elements, start writing logic to listen for click events on those elements
+	3c. You will also need a variable to keep track of moves - this will be used to indicate whether or not to draw an X or an O.
+
+REQUIREMENTS
+-Solution should use ONLY jQuery or vanilla JavaScript - not some combination thereof
+-A user should be able to click on different squares to make a move
+-Every click will alternate between marking an X and O
+-Upon marking of an individual cell, use JavaScript to add a class to each cell to display separate colors (either background or text-both is better)
+-A cell should not be able to be replayed once marked
+-Reset button clears the contents of the board
+
+BONUS
+-Display a message at the beginning of the game telling X or O to go first.
+-Alert the winner if they have won three in a row. Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
+-Add a scoreboard!
+*/ 
+
+console.log("JS is loaded!");
