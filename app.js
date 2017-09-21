876275c8945cd7d7f1504341464a535f8a5191fe
Courtney Fay <fay.courtney@gmail.com>
2017-09-20 19:20:05 -0600

    creates a function that can tell whether the number of clicks is odd or even in vanilla JS

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ Hints and Tricks
 	3a. Locate the element first to use it within your app. 
 		Think about using document.getElementById, document.getElementsByClassName or 
 		something similar to locate your target elements. 
-		Try this in your console to make sure your selection works.																DONE
-	3b. After finding the elements, start writing logic to listen for click events on those elements
-	3c. You will also need a variable to keep track of moves - this will be used to indicate whether or not to draw an X or an O.
+		Try this in your console to make sure your selection works.																	DONE
+	3b. After finding the elements, start writing logic to listen for click events on those elements 								DONE
+	3c. You will also need a variable to keep track of moves - this will be used to indicate whether or not to draw an X or an O. 	
 
 REQUIREMENTS
 -Solution should use ONLY jQuery or vanilla JavaScript - not some combination thereof
@@ -15,7 +15,7 @@ REQUIREMENTS
 -Upon marking of an individual cell, use JavaScript to add a class to each cell 
 	to display separate colors (either background or text-both is better)
 -A cell should not be able to be replayed once marked
--Reset button clears the contents of the board																					DONE
+-Reset button clears the contents of the board																						DONE
 
 BONUS
 -Display a message at the beginning of the game telling X or O to go first.
@@ -26,6 +26,9 @@ BONUS
 
 console.log("JS is loaded!");
 //is there a vanilla JS equivalent to .ready() ?
+
+// global variable to store number of clicks
+let numberOfClicks = 0;
 
 // global variables to grab individual cell IDs
 let topLeftCell = document.getElementById('top-left');
@@ -39,9 +42,9 @@ let bottomMiddleCell = document.getElementById('bottom-middle');
 let bottomRightCell = document.getElementById('bottom-right');
 
 // creates an array of all the cells which can be looped through with one event listener
-let arrayOfAllCells = [];
+/*let arrayOfAllCells = [];
 arrayOfAllCells.push(topLeftCell, topMiddleCell, topRightCell);
-console.log(arrayOfAllCells);
+console.log(arrayOfAllCells);*/
 
 // global variables to grab row, column and diagonal classes
 let leftColumn = document.getElementsByClassName('left-column');
@@ -53,23 +56,36 @@ let rightRow = document.getElementsByClassName('right-row');
 let leftDiagonal = document.getElementsByClassName('left-diagonal');
 let rightDiagonal = document.getElementsByClassName('right-diagonal');
 
-//3b. After finding the elements, start writing logic to listen for click events on those elements
-// find the individual cell (so 9) and create event listeners on each of them
-//first go round, just console log that something is happening to the cell
-		
-topLeftCell.addEventListener("click", cellClickX); 
-topMiddleCell.addEventListener("click", cellClickX); 
-topRightCell.addEventListener("click", cellClickX); 
-middleLeftCell.addEventListener("click", cellClickX); 
-middleMiddleCell.addEventListener("click", cellClickX); 
-middleRightCell.addEventListener("click", cellClickX); 
-bottomLeftCell.addEventListener("click", cellClickX); 
-bottomMiddleCell.addEventListener("click", cellClickX); 
-bottomRightCell.addEventListener("click", cellClickX); 
+// each individual cell listens for a "click" event and then runs the function cellClick 		
+topLeftCell.addEventListener("click", cellClick); 
+topMiddleCell.addEventListener("click", cellClick); 
+topRightCell.addEventListener("click", cellClick); 
+middleLeftCell.addEventListener("click", cellClick); 
+middleMiddleCell.addEventListener("click", cellClick); 
+middleRightCell.addEventListener("click", cellClick); 
+bottomLeftCell.addEventListener("click", cellClick); 
+bottomMiddleCell.addEventListener("click", cellClick); 
+bottomRightCell.addEventListener("click", cellClick); 
 
-function cellClickX() {
-	console.log(event.type);
-	this.textContent = "X";
+function cellClick() {
+	numberOfClicks = numberOfClicks + 1;
+	
+	// finds the remainder to tell whether number of clicks is even or odd (determines whether X or O should appear)
+	let remainder = numberOfClicks % 2;
+	console.log("remainder " + remainder);
+
+	//if number of clicks is even (using remainder % trick), then change box to an X
+	if (remainder == 0) {
+		this.textContent = "X";
+	} 
+	//if number of clicks is odd, then change box to an O
+	else if (remainder == 1) {
+		this.textContent = "O";
+	} else
+	{
+		console.log("the number of clicks is neither odd nor even");
+	}
+	console.log("current number of clicks: " + numberOfClicks);
 };
 
 //could you use an array of individual cells to keep this DRY?
